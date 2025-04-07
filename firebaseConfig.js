b2a350674ef745baa2a23a5533083fc57c6b4ce1
Phalanndwa Munyai <126163646+ST10356476@users.noreply.github.com>
2025-04-07 10:47:31 +0200

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,12 +6,12 @@ import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
     apiKey: "",
-    authDomain: "flashgenius-fd78d.firebaseapp.com",
-    projectId: "flashgenius-fd78d",
-    storageBucket: "flashgenius-fd78d.appspot.com",
-    messagingSenderId: "1028067672049",
-    appId: "1:1028067672049:web:01f4b273dbbfa6c462ce50",
-    measurementId: "G-50B5N50ZTB"
+    authDomain: 
+    projectId: 
+    storageBucket: 
+    messagingSenderId: 
+    appId: 
+    measurementId: 
 };
 
 const app = initializeApp(firebaseConfig);
